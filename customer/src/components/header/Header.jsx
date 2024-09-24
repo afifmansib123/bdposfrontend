a9c0e01b8899681b038e6591223aade1ce92adfa
afifmansib123 <78995381+afifmansib123@users.noreply.github.com>
2024-09-24 17:55:31 +0700

    Update Header.jsx

--- customer/src/components/header/Header.jsx
+++ customer/src/components/header/Header.jsx
@@ -31,7 +31,7 @@ const Header = ({ setSearch }) => {
       <header className="py-4 px-6 flex justify-between items-center gap-10">
         <div className="logo">
           <Link to="/">
-            <h2 className="text-2xl font-bold md:text-4xl">POSitive</h2>
+            <h2 className="text-2xl font-bold md:text-4xl">BDPOS</h2>
           </Link>
         </div>
         <div
